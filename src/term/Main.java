0a21c6ff9471fa1597219b97e34550a6b0385494
marrodri <laza.mar64@gmail.com>
2024-02-12 19:48:05 -0800

    main menu base implementation

---
+++ src/term/Main.java
@@ -0,0 +1,10 @@
+package term;
+
+public class Main {
+    public static void main(String[] args) {
+        App app = new App();
+        app.runApp();
+      int num = 0;
+//      System.out.println("Random number: " + num);
+    }  
+}
